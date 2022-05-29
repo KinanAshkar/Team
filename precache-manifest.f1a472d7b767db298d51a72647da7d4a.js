abfdf044d0c8503e5cf9a731732d673650f736fb
Kinan Ashkar <kinan-ashkar@live.com>
2022-05-29 19:09:05 +0200

    Updates

---
+++ precache-manifest.f1a472d7b767db298d51a72647da7d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcf4180a084d70bfd046",
+    "url": "/team/static/css/main.a8a2e576.chunk.css"
+  },
+  {
+    "revision": "bcf4180a084d70bfd046",
+    "url": "/team/static/js/main.bcf4180a.chunk.js"
+  },
+  {
+    "revision": "6f978d6d20c1b6d21f5b",
+    "url": "/team/static/js/1.6f978d6d.chunk.js"
+  },
+  {
+    "revision": "b5073e296f0e5daab715",
+    "url": "/team/static/js/runtime~main.b5073e29.js"
+  },
+  {
+    "revision": "543be332eff1baafe53fc5c1ffdf244b",
+    "url": "/team/index.html"
+  }
+];
